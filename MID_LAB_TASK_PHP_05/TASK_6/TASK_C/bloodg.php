b5084298ee81b3300e54c9a07fa034cdb3efe5ca
AZIM UDDIN <ashafin758@gmail.com>
2021-02-17 00:05:52 +0600

    taskC

---
+++ MID_LAB_TASK_PHP_05/TASK_6/TASK_C/bloodg.php
@@ -0,0 +1,45 @@
+<?php
+
+    $blood = "";
+
+    if(isset($_POST['submit'])){
+        $blood = $_POST['bloodGroup'];
+    }
+
+    else{
+        $blood = "";
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Group</title>
+</head>
+<body>
+    <form method="POST" action="#">
+        <fieldset style="width:200px">
+            <legend><b>BLOOD GROUP</b></legend>
+            <select name="bloodGroup">
+                <option value=""> </option>
+				<option value="A+">A+</option>
+				<option value="A-">A-</option>
+				<option value="B+">B+</option>
+				<option value="B-">B-</option>
+				<option value="AB+">AB+</option>
+				<option value="AB-">AB-</option>
+				<option value="O+">O+</option>
+				<option value="O-">O-</option>
+			</select><br>
+            <input type="text" name="blood" value="<?=$blood?>">
+			<hr>
+       <input type="submit" name="submit" value="Submit">
+        </fieldset>
+        <br>
+    </form>
+</body>
+</html>
