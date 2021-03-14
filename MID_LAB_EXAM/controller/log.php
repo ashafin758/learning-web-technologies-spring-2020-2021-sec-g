a18da4343c449d97971274edb46092363f7fae3a
AZIM UDDIN <ashafin758@gmail.com>
2021-03-14 11:17:22 +0600

    3rd commit at 11:17 PM

--- MID_LAB_EXAM/controller/log.php
+++ MID_LAB_EXAM/controller/log.php
@@ -4,26 +4,30 @@
         $inp = file_get_contents('../model/user.json');
 		$temp = json_decode($inp, true);
 
-        if($_POST['id'] == "" || $_POST['pass'] == ""){
+        if($_POST['id'] == "" || $_POST['pass'] == "")
+        {
 			echo "null submission...";
 		}
 		else{
 			$f=0;
 			foreach ($temp as $key1 => $value1) {
-				if ($temp[$key1]["id"]==$_POST['id'] and $temp[$key1]["pass"]==$_POST['pass']) {
+				if ($temp[$key1]["id"]==$_POST['id'] and $temp[$key1]["pass"]==$_POST['pass']) 
+				{
 					$f=1;
                     $_SESSION['nam']=$temp[$key1]["name"];
                     $type=$temp[$key1]["type"];
 				}
 
 			}
-			if($f==1){
+			if($f==1)
+			{
 				$_SESSION['flag'] = true;
 				$name=$_POST['username'];
 				
 				header('location: ../view/home.php');
 			}
-			else{
+			else
+			{
 				echo"invaild user";
 			}
 		}
