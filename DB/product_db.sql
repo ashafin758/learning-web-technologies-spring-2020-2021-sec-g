f5b9a4426757fd84bf09dcc0a7feda75b4c4f491
AZIM UDDIN <ashafin758@gmail.com>
2021-03-23 10:33:07 +0600

    database update1

--- DB/product_db.sql
+++ DB/product_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2021 at 05:20 AM
+-- Generation Time: Mar 23, 2021 at 05:32 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -44,7 +44,8 @@ INSERT INTO `products` (`ID`, `name`, `buyingPrice`, `sellingPrice`, `profit`) V
 (2, 'Nokia', '14000', '18000', '4000'),
 (3, 'HTC', '15000', '20000', '5000'),
 (4, 'Apple', '50000', '60000', '10000'),
-(5, 'Xiaomi', '28000', '30000', '2000');
+(5, 'Xiaomi', '28000', '30000', '2000'),
+(6, 'Realme', '15000', '16000', '1000');
 
 --
 -- Indexes for dumped tables
@@ -64,7 +65,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
